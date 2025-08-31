7c511205f6152de35a4c0ad7aaafd5c69c8b56e6
Sofie <sov.002@edu.zealand.dk>
2025-08-31 23:47:05 +0200

    bwdwdw

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
-$user = "root"; // ret til din db-bruger
-$pass = " ";     // ret til dit password
+$user = "root";
+$pass = ""; 
 $dbname = "flashcards_db";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
@@ -10,8 +10,3 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
-<!-- 
-// $host = "mysql18.unoeuro.coms";
-// $user = "sofieroshni_dk"; // ret til din db-bruger
-// $pass = "R4FA35tEDcgk2xe6fGbd ";     // ret til dit password
-// $dbname = "flashcards_db"; -->
